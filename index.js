55870c511ec660ff2b2c5acf8547c38aea6bd42f
Marc Satchmo <marcsatchmo@gmail.com>
2022-01-30 00:28:29 -0300

    actualizaciones varias

--- index.js
+++ index.js
@@ -1,14 +1,34 @@
-var app = angular.module('loginApp', []);+
+var appLogin = angular.module('loginApp', [])
 
-app.controller('loginCtrl', function($scope, $window){
+appLogin.controller('loginCtrl', ['$scope', '$window', '$http', function($scope, $window, $http){
     $scope.Loguear = function() {
-        console.log("entro");
-        $window.location.href = 'home.html';
-        // if($scope.user == 'admin' && $scope.password == 'admin') {
-        //     console.log("entro al if submit")
-        //     $window.location.href = 'home.html';
-        // } else {
-        //     alert('Wrong Stuff');
-        // }
+        var request={ 
+            usuario: $scope.user, 
+            clave: $scope.password 
+        };
+
+        var headers = {
+            'Access-Control-Allow-Origin': true,
+            'Content-Type': 'application/json; charset=utf-8',
+            "X-Requested-With": "XMLHttpRequest"
+        }
+        
+        $http({
+            method: 'POST',
+            url: 'https://localhost:44344/api/CasoDeUsoIniciarAplicacion/IniciarSesion',
+            params: request,
+            headers: headers
+        }).then(function successCallback(response) {
+            if(response) {
+                console.log("entro al if submit")
+                $window.location.href = 'home.html';
+            } else {
+                alert('Wrong Stuff');
+            }
+              // when the response is available
+            }, function errorCallback(response) {
+              // called asynchronously if an error occurs
+              // or server returns response with an error status.
+        });
     }
-});
+}]);
